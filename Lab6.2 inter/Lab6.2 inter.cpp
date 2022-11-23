589b7e6c90cc3017c9db70db35d72292cc84bf68
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-24 01:14:49 +0200

    nt

---
+++ Lab6.2 inter/Lab6.2 inter.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+void create(int* a, const int n, const int HIGH, const int LOW);
+void print(int* a, const int n);
+double calculate(int* a, const int n, const int i, double& serednie_arif,
+	int& all_i_elements, int& amount); 
+
+int main()
+{
+	srand(time(NULL));
+
+	int n;
+	cout << "A: " << endl;
+	cout << "n = "; cin >> n;
+	int* a = new int[n];
+	const int HIGH = 53;
+	const int LOW = -17;
+	create(a, n, HIGH, LOW);
+	print(a, n);
+	double serednie_arif = 0;
+	int amount = 0;
+	int all_i_elements = 0;		
+	cout << "F = "
+		<< calculate(a, n, 0, serednie_arif, amount, all_i_elements) << endl;
+	delete[] a;
+}
+void create(int* a, const int n, const int HIGH, const int LOW)
+{
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = LOW + rand() % (HIGH - LOW + 1);
+	}
+}
+void print(int* a, const int n)
+{
+	cout << "a[] = { ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << "}" << endl;
+}
+double calculate(int* a, const int n, const int i, double& serednie_arif,
+	int& all_i_elements, int& amount)
+{
+	bool umova = false;
+	for (int i = 0; i < n;)
+	{
+		if (a[i] % 2 == 0)
+		{
+			umova = true;
+			all_i_elements++;
+			amount += i;
+		}
+		if (umova)
+		{
+			serednie_arif = (1. * amount) / all_i_elements;
+		}
+		i++;
+	}
+	return serednie_arif;
+}
---
+++ Lab6.2inter test/Lab6.2inter test.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.2 inter/Lab6.2 inter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab62intertest
+{
+	TEST_CLASS(Lab62intertest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t = 0;
+			int arr[8] = { 1, -4, 4, 6, 2, 0, -3, -2 };
+			int amount = 0;
+			double serednie_arif = 0;
+			int all_i_elements = 0;
+			t = calculate(arr, 8, 0, serednie_arif, all_i_elements, amount);
+			Assert::AreEqual(t, 3.6667, 0.0001);
+		}
+	};
+}
